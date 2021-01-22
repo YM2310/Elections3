4dbee459f931cd1e196f9fdebd93a287477bbd7b
Shirley Fichman <69894229+ShirleyFichman@users.noreply.github.com>
2021-01-22 21:39:07 +0200

    Update DistrictArr.h

--- Elections3/DistrictArr.h
+++ Elections3/DistrictArr.h
@@ -2,6 +2,7 @@
 #include "WTADistrict.h"
 #include "RelativeDistrict.h"
 #include "DistrictPtr.h"
+#include <map>
 
 class DistrictArr
 {
@@ -12,19 +13,17 @@ public:
 	~DistrictArr();
 	
 	/*Getters:*/
-	int getLogSize() const;
+
 	const District& getDistrict(int district_num) const;
 	const Citizen* getCitizen(int id, int district_num = -1)const;
 
 	/*Adders:*/
-	District* addDistrict(myString& name, int district_id, int electors, DistrictType type);
-	int addCitizen(myString& name, int id, int birthyear, int district_id);
+	
+	District* addDistrict(string& name, int district_id, int electors, DistrictType type);
+	int addCitizen(string& name, int id, int birthyear, int district_id);
 	void addParty(const Party* partynum);
 	int addVote(int party_num, int id);
 	int addRep(int party_num, int id, int district_id);
-
-	/*Setters: */
-	void changeSize(int size);
 
 	/*Files: */
 	void saveDistricts(ostream& out)const;
@@ -35,13 +34,9 @@ public:
 	void printDistricts() const;
 
 	/*Operators: */
-	District& operator[](int i) { return *district_arr[i]; }
-	const District& operator[](int i) const { return *district_arr[i]; }
 	friend ostream& operator<<(ostream& os, const DistrictArr& arr);
-	
-private:
-	void doubleSize();
-	DistrictPtr* district_arr;
-	int log_size;
-	int real_size;
-};
+
+	/*STL: */
+	map<int, District*> district_map;
+
+}; 
