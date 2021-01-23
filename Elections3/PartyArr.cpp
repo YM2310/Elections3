6a7a34216ae867339e3017f0ff48a0c0cf621aa4
Shirley Fichman <69894229+ShirleyFichman@users.noreply.github.com>
2021-01-23 22:48:40 +0200

    Update PartyArr.cpp

--- Elections3/PartyArr.cpp
+++ Elections3/PartyArr.cpp
@@ -3,199 +3,175 @@
 #define rcastcc reinterpret_cast<const char*>
 #define rcastc reinterpret_cast<char*>
 
-PartyArr::PartyArr(int _logsize, int _realsize)
+PartyArr::PartyArr(int size) : party_arr(size)
 {
-	log_size = _logsize;
-	real_size = _realsize;
-	party_arr = new PartyPtr[real_size];
+
 }
 
 PartyArr::PartyArr(istream& in, DistrictArr& district_map)// load
 {
-	in.read(rcastc(&log_size), sizeof(log_size));
-	real_size = log_size + 1;
-	party_arr = new PartyPtr[real_size];
-	for (int i = 0; i < log_size; i++)
-	{
-		Party* party = new Party(in, district_map);
-		party_arr[i].setPtr(party);
+	try {
+		int log_size;
+		in.read(rcastc(&log_size), sizeof(log_size));
+		for (int i = 0; i < log_size; i++)
+		{
+			Party* party = new Party(in, district_map);
+			party_arr.push_back(party);
+		}
+	}
+	catch (istream::failure& ex) {
+		throw("Exception opening/reading/closing file");
+	}
+	catch (bad_alloc& ex) {
+		throw;
 	}
 }
 
 
 PartyArr::~PartyArr()
 {
-	delete[] party_arr;
+
 }
 
 int PartyArr::getLogSize() const
 {
-	return(log_size);
+	return(party_arr.size());
 }
 
-Party& PartyArr::getParty(int partyNum) const
+const Party& PartyArr::getParty(int partyNum) const
 {
-	for (int i = 0; i < log_size; i++)
+	DynamicArray<PartyPtr>::const_iterator iter = party_arr.begin();
+	DynamicArray<PartyPtr>::const_iterator end = party_arr.end();
+	for (auto& party : party_arr)
 	{
-		if (party_arr[i]->getPartyNum() == partyNum)
-			return *party_arr[i];
+		if (party->getPartyNum() == partyNum)
+			return *party;
 	}
+	throw "No party with this party id";
 }
 
 Party* PartyArr::addParty(string& partyName, int _partyNum, const Citizen* leader)
 {
-	if (log_size == real_size)
-		doubleSize();
 	Party* new_party = new Party(partyName, leader, _partyNum);
-	party_arr[log_size].setPtr(new_party);
-
-	log_size++;//so the first one added to PartyArr will be in idx 0
+	PartyPtr temp_ptr(new_party);
+	party_arr.push_back(temp_ptr);
+	temp_ptr = nullptr;
 	return new_party;
 }
 
 void PartyArr::addDistrict(District* district_id)
 {
-	for (int i = 0; i < log_size; i++)
-	{
-		party_arr[i]->addDistrict(district_id);
-	}
-	PartyPtr temp;
-}
-
-void PartyArr::doubleSize()
-{
-	changeSize(real_size * 2);
-}
-
-int PartyArr::CheckIfRep(const Citizen* newrep)const { //gets a citizen we know exists and returns if they are a rep of a party
-	for (int j = 0; j < log_size; j++)
-	{
-		for (int m = 0; m < party_arr[j]->getRepsArr().getLogSize(); m++)
+	try {
+		for (auto& party : party_arr)
 		{
-			for (int n = 0; n < party_arr[j]->getRepsArr()[m].getLogSize(); n++)
+			party->addDistrict(district_id);
+		}
+	}
+	catch (...) {
+		throw;
+	}
+}
+
+
+
+void PartyArr::CheckIfRep(const Citizen* newrep)const { //gets a citizen we know exists and returns if they are a rep of a party
+	for (auto& party : party_arr)
+	{
+		if (party->getLeader().getID() == newrep->getID())
+			throw"This citizen is already a representative";
+		for (auto& reps_of_dist : party->getRepsArr().getReps())
+		{
+			for (auto& rep : reps_of_dist.getAllReps())
 			{
-				if (party_arr[j]->getRepsArr()[m].getIDRep(n) == newrep->getID())
+				if (rep->getID() == newrep->getID())
 				{
-					return 100;
+					throw"This citizen is already a representative";
 				}
 			}
 		}
-		if (party_arr[j]->getLeader().getID() == newrep->getID())
-			return 100;
 	}
-	return 200;// not a rep 
+	return;
 }
 
-int PartyArr::addRep(int party_num, int district_num, const Citizen* newrep)
+void PartyArr::addRep(int party_num, int district_num, const Citizen* newrep)
 {
-	if (CheckIfRep(newrep) == 200)
-	{
-		for (int i = 0; i < log_size; i++)
+	try {
+		CheckIfRep(newrep);
+		for (auto& party : party_arr)
 		{
-			if (party_arr[i]->getPartyNum() == party_num)
+			if (party->getPartyNum() == party_num)
 			{
-				return party_arr[i]->addRep(newrep, district_num);
+				return party->addRep(newrep, district_num);
 			}
 		}
 	}
-	else
-		return 100; //This citizen is already a representative
+	catch (...) {
+		throw;
+	}
+
 }
 
 void PartyArr::addElectoralVotes(int party_num, int electors)
 {
-	for (int i = 0; i < log_size; i++)
+	for (auto& party : party_arr)
 	{
-		if (party_arr[i]->getPartyNum() == party_num)
+		if (party->getPartyNum() == party_num)
 		{
-			party_arr[party_num]->addElectors(electors);
+			party->addElectors(electors);
 		}
 	}
 }
 
 void PartyArr::addVotes(int party_num, int votes)
 {
-	for (int i = 0; i < log_size; i++)
+	for (auto& party : party_arr)
 	{
-		if (party_arr[i]->getPartyNum() == party_num)
+		if (party->getPartyNum() == party_num)
 		{
-			party_arr[party_num]->updateVotes(votes);
+			party->updateVotes(votes);
 		}
-	}
-}
-
-int PartyArr::partition(PartyArr& arr, int left, int right)
-{
-	Party* tmp;
-	Party& pivot = arr[right];
-	int i = (left - 1);
-
-	for (int j = left; j < right; j++)
-	{
-		if (arr[j].getElectorsWon() > pivot.getElectorsWon())
-		{
-			i++;
-			tmp = &arr[i];
-			arr.party_arr[i].setPtr(&arr[j]);
-			arr.party_arr[j].setPtr(tmp);
-
-		}
-		if (arr[j].getElectorsWon() == pivot.getElectorsWon())
-		{
-			if (arr[j].getPartyNum() < pivot.getPartyNum())//handles daw
-			{
-				i++;
-				tmp = &arr[i];
-				arr.party_arr[i].setPtr(&arr[j]);
-				arr.party_arr[j].setPtr(tmp);
-			}
-		}
-	}
-	tmp = &arr[i + 1];
-	arr.party_arr[i + 1].setPtr(&arr[right]);
-	arr.party_arr[right].setPtr(tmp);
-	return (i + 1);
-}
-
-void PartyArr::quickSort(PartyArr& arr, int left, int right)
-{
-	if (left < right)
-	{
-		int pivot = partition(arr, left, right);
-		quickSort(arr, left, pivot - 1);
-		quickSort(arr, pivot + 1, right);
 	}
 }
 
 void PartyArr::save(ostream& out) const
 {
-	out.write(rcastcc(&log_size), sizeof(log_size));
-	for (int i = 0; i < log_size; ++i) {
-		party_arr[i]->save(out);
+	try {
+		int log_size = party_arr.size();
+		out.write(rcastcc(&log_size), sizeof(log_size));
+		for (auto party : party_arr) {
+			party->save(out);
+		}
+	}
+	catch (ostream::failure& ex) {
+		throw("Exception opening/reading/closing file");
 	}
 }
 
 void PartyArr::load(istream& in, DistrictArr& district_map)
 {
-	int loaded_size;
-	in.read(rcastc(&loaded_size), sizeof(loaded_size));
-	real_size = loaded_size + 1;
-	changeSize(real_size);
-	log_size = loaded_size;
+	try {
+		int loaded_size;
+		in.read(rcastc(&loaded_size), sizeof(loaded_size));
 
-	for (int i = 0; i < log_size; i++)
-	{
-		party_arr[i].deleteParty();
-		Party* party = new Party(in, district_map);
-		party_arr[i].setPtr(party);
+		for (int i = 0; i < loaded_size; i++)
+		{
+			Party* party = new Party(in, district_map);
+			party_arr.push_back(party);
+		}
+	}
+	catch (istream::failure& ex) {
+		throw("Exception opening/reading/closing file");
+	}
+	catch (bad_alloc& ex) {
+		throw;
 	}
 }
 
 void PartyArr::initElectors()
 {
-	for (int i = 0; i < log_size; i++)
+	for (auto& party : party_arr)
 	{
-		party_arr[i]->resetElectors();
+		party->resetElectors();
 	}
 }
 
@@ -206,60 +182,4 @@ ostream& operator<<(ostream& os, const PartyArr& arr)
 		os << arr[i];
 	}
 	return os;
-}
-
-int PartyArr::partitionByVotes(PartyArr& arr, int left, int right)
-{
-	Party* tmp;
-	Party& pivot = arr[right];
-	int i = (left - 1);
-
-	for (int j = left; j < right; j++)
-	{
-		if (arr[j].getVotes() > pivot.getVotes())
-		{
-			i++;
-			tmp = &arr[i];
-			arr.party_arr[i].setPtr(&arr[j]);
-			arr.party_arr[j].setPtr(tmp);
-
-		}
-		if (arr[j].getVotes() == pivot.getVotes())
-		{
-			if (arr[j].getPartyNum() < pivot.getPartyNum())//handles daw
-			{
-				i++;
-				tmp = &arr[i];
-				arr.party_arr[i].setPtr(&arr[j]);
-				arr.party_arr[j].setPtr(tmp);
-			}
-		}
-	}
-	tmp = &arr[i + 1];
-	arr.party_arr[i + 1].setPtr(&arr[right]);
-	arr.party_arr[right].setPtr(tmp);
-	return (i + 1);
-}
-
-void PartyArr::changeSize(int size)
-{
-	real_size = size;
-	PartyPtr* new_size = new PartyPtr[real_size];
-	memcpy(new_size, party_arr, sizeof(PartyPtr) * log_size);
-	for (int i = 0; i < log_size; i++)
-	{
-		party_arr[i].setPtr(nullptr);
-	}
-	delete[] party_arr; // <-- THIS
-	party_arr = new_size; // <-- THIS
-}
-
-void PartyArr::quickSortByVotes(PartyArr& arr, int left, int right)
-{
-	if (left < right)
-	{
-		int pivot = partitionByVotes(arr, left, right);
-		quickSortByVotes(arr, left, pivot - 1);
-		quickSortByVotes(arr, pivot + 1, right);
-	}
 }
