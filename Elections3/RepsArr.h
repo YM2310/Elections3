f29a7f62d08a18b6c57b61413c4b6f6938de8134
Shirley Fichman <69894229+ShirleyFichman@users.noreply.github.com>
2021-01-23 22:53:20 +0200

    Update RepsArr.h

--- Elections3/RepsArr.h
+++ Elections3/RepsArr.h
@@ -1,29 +1,30 @@
 #include "Reps.h"
+#include "DynamicArray.h"
 class District;
 class DistrictArr;
 class RepsArr
 {
 public:
-	RepsArr(int size = 1);
+	RepsArr(int size = 2);
 	~RepsArr();
 
 	void addDistrict(const District* district_id);
-	int addRep(const Citizen* rep, int district_id);
+	void addRep(const Citizen* rep, int district_id);
 	void copyReps(Reps* origin, int origin_size);
 	/*Getters: */
 	int getLogSize() const;
 	const Reps& getRepsOfDistrict(int district_num) const;
+	const DynamicArray<Reps>& getReps() const {
+		return reps;
+	}
 
 	/*Files: */
 	void save(ostream& out) const;
 	void load(istream& in, const DistrictArr& district_map);
 
 	const Reps& operator[](int i) const { return reps[i]; }
-	
+
 private:
-	Reps* reps;
-	int log_size;
-	int real_size;
-	void doubleSize();
-	void changeSize(int size);
+	DynamicArray<Reps> reps;
+
 };
